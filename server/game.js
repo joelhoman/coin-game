4e7a39b825f4bce074ca2ff5212cd5bbb24c67f3
joelhoman <jhoman3@lion.lmu.edu>
2017-02-15 22:36:31 -0800

    finally linted code and integrated move with redis

--- server/game.js
+++ server/game.js
@@ -6,22 +6,21 @@ const HEIGHT = 64;
 const MAX_PLAYER_NAME_LENGTH = 32;
 const NUM_COINS = 100;
 
-const database = {
-  scores: {},
-  usednames: new Set(),
-  coins: {},
-};
+// const database = {
+//   scores: {},
+//   usednames: new Set(),
+//   coins: {},
+// };
 
 exports.addPlayer = (name) => {
-  redis.sismember('usednames', name, function (err, reply) {
+  redis.sismember('usednames', name, (err, reply) => {
     if (name.length === 0 || name.length > MAX_PLAYER_NAME_LENGTH || reply) {
       return false;
-    } else {
-      redis.sadd('usednames', name);
-      redis.hset('players', `player:${name}`, randomPoint(WIDTH, HEIGHT).toString());
-      redis.zadd('scores', 0, name);
-      return true;
     }
+    redis.sadd('usednames', name);
+    redis.hset('players', name, randomPoint(WIDTH, HEIGHT).toString());
+    redis.zadd('scores', 0, name);
+    return true;
   });
   // database.usednames.add(name);
   // database[`player:${name}`] = randomPoint(WIDTH, HEIGHT).toString();
@@ -33,7 +32,8 @@ function placeCoins() {
   permutation(WIDTH * HEIGHT).slice(0, NUM_COINS).forEach((position, i) => {
     const coinValue = (i < 50) ? 1 : (i < 75) ? 2 : (i < 95) ? 5 : 10;
     const index = `${Math.floor(position / WIDTH)},${Math.floor(position % WIDTH)}`;
-    database.coins[index] = coinValue;
+    // database.coins[index] = coinValue;
+    redis.hset('coins', index, coinValue);
   });
 }
 
@@ -41,41 +41,92 @@ function placeCoins() {
 // the positions of each player, the scores, and the positions (and values) of each coin.
 // Note that we return the scores in sorted order, so the client just has to iteratively
 // walk through an array of name-score pairs and render them.
-exports.state = () => {
-  const positions = Object.entries(database)
-    .filter(([key]) => key.startsWith('player:'))
-    .map(([key, value]) => [key.substring(7), value]);
-  const scores = Object.entries(database.scores);
-  scores.sort(([, v1], [, v2]) => v1 < v2);
+
+function getPlayers() {
+  redis.hgetall('players', (err, reply) => ({
+    reply,
+  }));
+}
+
+function getScores() {
+  redis.zrevrange('scores', 0, -1, (err, reply) => ({
+    reply,
+  }));
+}
+
+function getCoins() {
+  redis.hgetall('coins', (err, reply) => ({
+    reply,
+  }));
+}
+
+function getState() {
   return {
-    positions,
-    scores,
-    coins: database.coins,
+    players: getPlayers(),
+    scores: getScores(),
+    coins: getCoins(),
   };
+}
+
+exports.state = () => {
+  // const positions = Object.entries(database)
+  //   .filter(([key]) => key.startsWith('player:'))
+  //    .map(([key, value]) => [key.substring(7), value]);
+  // const scores = Object.entries(database.scores);
+  getState((err, reply) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+    }
+    return reply;
+  });
 };
+
+function getPlayer(name) {
+  redis.hget('players', name, (err, reply) => ({
+    reply,
+  }));
+}
+
+function getCoin(location) {
+  redis.hget('coins', location, (err, reply) => ({
+    reply,
+  }));
+}
+
+function getNumCoins() {
+  redis.hlen('coins', (err, reply) => ({
+    reply,
+  }));
+}
 
 exports.move = (direction, name) => {
   const delta = { U: [0, -1], R: [1, 0], D: [0, 1], L: [-1, 0] }[direction];
   if (delta) {
-    const playerKey = `player:${name}`;
-    const [x, y] = database[playerKey].split(',');
+    // const playerKey = `player:${name}`;
+    // const [x, y] = database[playerKey].split(',');
+    const [x, y] = getPlayer(name).split(',');
     const [newX, newY] = [clamp(+x + delta[0], 0, WIDTH - 1), clamp(+y + delta[1], 0, HEIGHT - 1)];
-    const value = database.coins[`${newX},${newY}`];
+    const playerLocation = `${newX},${newY}`;
+    // const value = database.coins[`${newX},${newY}`];
+    const value = getCoin(playerLocation);
     if (value) {
-      database.scores[name] += value;
-      delete database.coins[`${newX},${newY}`];
+      // database.scores[name] += value;
+      redis.zincrby('scores', value, name);
+      // delete database.coins[`${newX},${newY}`];
+      redis.hdel('coins', playerLocation);
     }
-    database[playerKey] = `${newX},${newY}`;
+    // database[playerKey] = `${newX},${newY}`;
+    redis.hset('players', name, playerLocation);
 
     // When all coins collected, generate a new batch.
-    if (Object.keys(database.coins).length === 0) {
+    if (!getNumCoins) {
       placeCoins();
     }
   }
 };
 
 redis.on('error', (err) => {
-  console.error('Error: ${err}');
+  console.error(`Error: ${err}`);
 });
 
 placeCoins();
